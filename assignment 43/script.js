3ab1926c69e178a8fdf51077fcd81262daf2c2b8
Eshal Irfan <155638363+eshal-irfan10@users.noreply.github.com>
2024-08-31 08:33:58 -0700

    Add files via upload

---
+++ assignment 43/script.js
@@ -0,0 +1,69 @@
+// function showAlert() {
+//     alert("You clicked the link!");
+// }
+// function iphone() {
+//     alert("Thank you for buying our iphone ")
+// }
+// function phone() {
+//     alert("Thank you for buying our iphone ")
+// }
+// function pinkiphone() {
+//     alert("Thank you for buying our iphone ")
+// }
+// function iphone_() {
+//     alert("Thank you for buying our iphone ")
+// }
+// function _iphone() {
+//     alert("Thank you for buying our iphone ")
+// }
+// function i_phone() {
+//     alert("Thank you for buying our iphone ")
+// }
+// function _phone() {
+//     alert("Thank you for buying our iphone ")
+// }
+// function phone_() {
+//     alert("Thank you for buying our iphone ")
+// }
+// function deleteRow(button) {
+//     var row = button.parentNode.parentNode;
+//     row.parentNode.removeChild(row);
+// }
+// const image = document.getElementById('hoverImage');
+
+// image.addEventListener('mouseover', function() {
+//     image.src = 'https://i.pinimg.com/originals/77/8d/87/778d8713deefff6f96cdd6b6ed588042.jpg';
+// });
+
+// image.addEventListener('mouseout', function() {
+//     image.src = 'https://www.kfcpakistan.com/images/33685b40-0461-11ee-911c-497570899609-Mighty_variant_0-2023-06-06115641.png';
+//     image.style.width = 200
+// });
+
+// let count = 0;
+// const counterDisplay = document.getElementById('counter');
+// const increaseButton = document.getElementById('increase');
+// const decreaseButton = document.getElementById('decrease');
+
+// increaseButton.addEventListener('click', function() {
+//     count++;
+//     counterDisplay.textContent = count;
+// });
+
+// decreaseButton.addEventListener('click', function() {
+//     count--;
+//     counterDisplay.textContent = count;
+// });
+
+
+    
+        
+
+
+
+
+
+
+
+
+
